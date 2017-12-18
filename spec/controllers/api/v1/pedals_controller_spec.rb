ff4fc2a0ba826b83153062eda0c38d851c0ed1ee
jsc215 <jeffcomertennis@gmail.com>
2017-12-17 19:09:00 -0500

    tests

--- spec/controllers/api/v1/pedals_controller_spec.rb
+++ spec/controllers/api/v1/pedals_controller_spec.rb
@@ -6,9 +6,17 @@ RSpec.describe Api::V1::PedalsController, type: :controller do
   describe 'GET index' do
     it 'should render json object of all pedals' do
       pedal = FactoryBot.create(:pedal)
+
       get :index
       json = JSON.parse(response.body)
+
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq('application/json')
+
       expect(json["pedals"][0]["name"]).to eq("Boss Blues Driver 1")
+      expect(json["pedals"][0]["description"]).to eq("best pedal in the world")
+      expect(json["pedals"][0]["effect_type"]).to eq("Overdrive")
+      expect(json["pedals"][0]["image_url"]).to eq("https://effektpedaler.dk/wp-content/uploads/dunlop-cry-baby-gcb95.jpg")
     end
   end
 end
--- spec/models/pedal_spec.rb
+++ spec/models/pedal_spec.rb
@@ -4,6 +4,11 @@ describe Pedal do
   it { should have_valid(:name).when('Boss Blues Driver') }
   it { should_not have_valid(:name).when(nil, '') }
 
-  it { should have_valid(:image_url).when('bluesdriver.com') }
+  it { should have_valid(:image_url).when('https://effektpedaler.dk/wp-content/uploads/dunlop-cry-baby-gcb95.jpg') }
   it { should_not have_valid(:image_url).when(nil, '') }
+
+  it { should have_valid(:effect_type).when('Overdrive') }
+
+  it { should have_valid(:description).when('awesome pedal') }
+
   end
