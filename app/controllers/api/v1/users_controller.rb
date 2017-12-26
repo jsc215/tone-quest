b490858c41b0b4679c267baec06c5369c0a6fe3d
jsc215 <jeffcomertennis@gmail.com>
2017-12-26 02:43:43 -0500

    code climate fixes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,8 +14,8 @@ class Api::V1::UsersController < ApplicationController
   end
 end
 
-  # private
-  #
-  #   def user_params
-  #     params.require(:user).permit(:name)
-#end
+# private
+#
+#  def user_params
+#   params.require(:user).permit(:name)
+# end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -3,8 +3,9 @@ require 'spec_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
   let!(:user) do
-    User.create!(
-        provider: 'MyString',
+    User.create!
+    (
+      provider: 'MyString',
       uid: 'MyString',
       first_name: 'MyString',
       last_name: 'MyString',
