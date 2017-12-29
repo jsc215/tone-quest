00763044fdf1cc8139d4e33b2f923ee4f7bd8cf5
jsc215 <jeffcomertennis@gmail.com>
2017-12-28 20:44:52 -0500

    merge master

--- spec/features/user_logs_in_with_google_spec.rb
+++ spec/features/user_logs_in_with_google_spec.rb
@@ -5,20 +5,21 @@ def stub_omniauth
   OmniAuth.config.test_mode = true
   # then, provide a set of fake oauth data that
   # omniauth will use when a user tries to authenticate:
-  OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({
-    provider: 'google',
-    uid: '12345678910',
-    info: {
-      email: 'jesse@mountainmantechnologies.com',
-      first_name: 'Jesse',
-      last_name: 'Spevack'
-    },
-    credentials: {
-      token: 'abcdefg12345',
-      refresh_token: '12345abcdefg',
-      expires_at: DateTime.now,
-    }
-  })
+  OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new(
+    {
+      provider: 'google',
+      uid: '12345678910',
+      info: {
+        email: 'jesse@mountainmantechnologies.com',
+        first_name: 'Jesse',
+        last_name: 'Spevack'
+      },
+      credentials: {
+        token: 'abcdefg12345',
+        refresh_token: '12345abcdefg',
+        expires_at: DateTime.now,
+      }
+    })
 end
 
 RSpec.feature 'user logs in' do
