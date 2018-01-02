e1d2eaf5bb8a5022ddbaba6c99d1db38339014da
jsc215 <jeffcomertennis@gmail.com>
2018-01-02 01:22:59 -0500

    added tests for pedalboards

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -2,17 +2,7 @@ require 'rails_helper'
 # require 'spec_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
-  # let!(:user) do
-  #   User.create!(
-  #     provider: 'MyString',
-  #     uid: 'MyString',
-  #     first_name: 'MyString',
-  #     last_name: 'MyString',
-  #     email: 'MyString',
-  #     token: 'MyString',
-  #     refresh_token: 'MyString'
-  #   )
-  # end
+
   describe 'GET#index' do
     it 'should render json object of all users' do
       user = FactoryBot.create(:user)
@@ -43,7 +33,6 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       expect(json['user']['first_name']).to eq('MyString')
       expect(json['user']['last_name']).to eq('MyString')
       expect(json['user']['email']).to eq('MyString')
-
     end
   end
 end
