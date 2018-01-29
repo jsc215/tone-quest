1f795593909bf0f1250a7c46efff990d60aa0ce9
jsc215 <jeffcomertennis@gmail.com>
2018-01-28 19:35:23 -0500

    updated tests for create actions

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,4 @@
-Api UsersController
+# Api UsersController
 class Api::V1::UsersController < ApplicationController
   # skip_before_action :verify_authenticity_token
   # before_action :authenticate_user!, except: [:show, :create]
@@ -7,16 +7,16 @@ class Api::V1::UsersController < ApplicationController
     users = User.all
     render json: { users: users, current_user: current_user }
   end
-end
 
-# #   def show
-# #     user = User.find(params[:id])
-# #     render json: user
-# #   end
-# # end
-#
-# private
-#
-#  def user_params
-#   params.require(:user).permit(:name)
-# end
+  # def show
+  #   user = User.find(params[:id])
+  #   render json: user
+  # end
+
+
+private
+
+ def user_params
+  params.require(:user).permit(:name)
+  end
+end
