9b0e041640e1f9c29a478d61f38a1e0430c7f19f
jsc215 <jeffcomertennis@gmail.com>
2018-01-28 19:57:36 -0500

    updated tests for create actions

---
+++ spec/controllers/pedalreviews_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+require 'spec_helper'
+
+RSpec.describe PedalreviewsController, type: :controller do
+  describe 'GET#new' do
+    it 'should render new template' do
+      get :new, params: { pedal_id: 1 }
+      expect(response).to render_template('pedalreviews/new')
+    end
+  end
+end
