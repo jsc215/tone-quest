0751507fbdcd936a4f572580dc526ff394afa317
jsc215 <jeffcomertennis@gmail.com>
2023-04-10 01:11:29 -0400

    working

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'omniauth-google-oauth2'
+gem 'omniauth'
+gem 'omniauth-rails_csrf_protection'
 gem 'figaro'
 gem 'faker'
 gem 'pry-rails'
@@ -50,4 +52,4 @@ end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-ruby '3.0.0'
+# ruby '2.5.0'
--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,9 +1,9 @@
 var config = {
   entry: {
-        path: __dirname + '/src/main.js',
+    path: __dirname + '/src/main.js'
   },
   output: {
-        path: __dirname + '/../app/assets/javascripts',
+    path: __dirname + '/../app/assets/javascripts',
     filename: 'bundle.js'
   },
   module: {
@@ -16,14 +16,12 @@ var config = {
     ]
   },
   devtool: 'eval-source-map'
-}
+};
 
 if (process.env.NODE_ENV === 'production') {
   delete config.devtool;
   var webpack = require('webpack');
-  config.plugins = [
-    new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' })
-  ];
+  config.plugins = [new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' })];
 }
 
 module.exports = config;
